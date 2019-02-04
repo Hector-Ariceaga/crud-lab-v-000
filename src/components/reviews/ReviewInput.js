5c1242fe5c30f1271bc563b18ed50c0e20bace69
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-04 22:27:08 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -14,7 +14,7 @@ class ReviewInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault()
-    this.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+    this.addReview({text: this.state.text, restaurantId: this.props.restaurant.id})
     this.setState({
       text: ''
     })
