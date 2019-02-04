d44ae66d6e6c573d17dc42cd5c387541948624b8
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-04 18:30:49 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,7 +3,7 @@ import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
 
-  const restaurants = () => this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} {...restaurant} deleteRestaurant={this.props.deleteRestaurant} />)
+  const restaurants = () => return this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} {...restaurant} deleteRestaurant={this.props.deleteRestaurant} />)
 
   render() {
     return(
