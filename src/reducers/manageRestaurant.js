061e000d6407096528c55540958a4f9de37af5f4
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-04 18:16:11 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,5 +2,13 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = {id: cuid(), text: action.text}
+      return {...state, restaurant}
+    case 'DELETE_RESTAURANT':
 
+    default:
+      return state:
+  }
 }
