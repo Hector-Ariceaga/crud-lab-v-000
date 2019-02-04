0bf5312a81b2634675837b97471dd455c174179c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-04 18:20:31 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -6,7 +6,7 @@ export default function manageRestaurants(state = {restaurants: []}, action) {
     case 'ADD_RESTAURANT':
       const restaurant = {id: cuid(), text: action.text}
       return {...state, restaurants: [...state.restaurants, restaurant]}
-      
+
     case 'DELETE_RESTAURANT':
       const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
       return {restaurants}
