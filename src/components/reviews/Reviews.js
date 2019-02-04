7cfccb9bd493a59522c4d49d26efe517fb669c2b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-04 23:55:42 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,12 +4,16 @@ import Review from './Review';
 class Reviews extends Component {
 
   render() {
-    console.log(this.props)
+  if(!this.props.reviews.empty?) {
     const restaurantReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
 
     const filteredReviews = restaurantReviews.map(review => {
       return <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
     })
+  } else {
+    return filteredReviews = <li>No reviews</li>
+  }
+    
 
     return (
       <ul>
