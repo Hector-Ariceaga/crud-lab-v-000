5cee0c46a34b35e26b8d93154f1f2229d4c88167
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-05 00:02:59 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,7 @@ class Reviews extends Component {
 
   render() {
     // const restaurantReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
-    // 
+    //
     // const filteredReviews = restaurantReviews.map(review => {
     //   return <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
     // })
