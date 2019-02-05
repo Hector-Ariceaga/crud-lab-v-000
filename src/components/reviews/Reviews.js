fe00002d7afa9ef2925471f175c6afc861d57382
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-05 00:44:46 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,11 +4,11 @@ import Review from './Review';
 class Reviews extends Component {
 
   render() {
-    // const restaurantReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
-    //
-    // const filteredReviews = restaurantReviews.map(review => {
-    //   return <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
-    // })
+    const restaurantReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    
+    const filteredReviews = restaurantReviews.map(review => {
+      return <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
+    })
 
     return (
       <ul>
