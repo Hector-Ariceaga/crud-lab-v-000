51f756d58ff858038e08d527b95cebe6a46ab1c6
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-05 00:46:14 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -9,7 +9,7 @@ export default function manageRestaurants(state = {restaurants: [], reviews: []}
 
     case 'DELETE_RESTAURANT':
       const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
-      return {restaurants}
+      return {...state, restaurants}
 
     case 'ADD_REVIEW':
       const review = {id: cuidFn(), restaurantId: action.review.restaurantId, text: action.review.text}
@@ -17,7 +17,7 @@ export default function manageRestaurants(state = {restaurants: [], reviews: []}
 
     case 'DELETE_REVIEW':
       const reviews = state.reviews.filter(review => review.id !== action.id)
-      return {reviews}
+      return {...state, reviews}
     default:
       return state;
   }
